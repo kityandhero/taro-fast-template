156b74516f2ab8676a9064af552a79623446b611
kityandhero <kityandhero@126.com>
2022-04-06 10:17:34 +0800

    refactor: update

--- src/app.config.js
+++ src/app.config.js
@@ -1,4 +1,4 @@
-export default {
+export default defineAppConfig({
   pages: ['pages/entry/main/index', 'pages/home/main/index'],
   window: {
     backgroundTextStyle: 'light',
@@ -19,4 +19,53 @@ export default {
     },
   },
   serviceProviderTicket: '',
-};
+  tabBar: {
+    color: '#353535',
+    selectedColor: '#3778F4',
+    borderStyle: 'black',
+    list: [
+      {
+        pagePath: 'pages/home/main/index',
+        iconPath: './assets/tab-bar/home.png',
+        iconPath: './assets/tab-bar/home-active.png',
+        text: '直播',
+      },
+      // {
+      //   pagePath: 'pages/live/index',
+      //   iconPath: './assets/tab-bar/home.png',
+      //   selectedIconPath: './assets/tab-bar/home-active.png',
+      //   text: '直播',
+      // },
+      // {
+      //   pagePath: "pages/productList/index",
+      //   iconPath: "./assets/tab-bar/cate.png",
+      //   selectedIconPath: "./assets/tab-bar/cate-active.png",
+      //   text: "分类"
+      // },
+      // {
+      //   pagePath: 'pages/home/index',
+      //   iconPath: './assets/tab-bar/cate.png',
+      //   selectedIconPath: './assets/tab-bar/cate-active.png',
+      //   text: '资讯',
+      // },
+      // {
+      //   pagePath: "pages/live/index",
+      //   iconPath: "./assets/tab-bar/live.png",
+      //   selectedIconPath: "./assets/tab-bar/live-active.png",
+      //   text: "直播"
+      // },
+      // {
+      //   pagePath: 'pages/cart/index',
+      //   iconPath: './assets/tab-bar/cart.png',
+      //   selectedIconPath: './assets/tab-bar/cart-active.png',
+      //   text: '天中集市',
+      // },
+      {
+        pagePath: 'pages/customer/index',
+        iconPath: './assets/tab-bar/user.png',
+        selectedIconPath: './assets/tab-bar/user-active.png',
+        text: '我的',
+      },
+    ],
+  },
+});
